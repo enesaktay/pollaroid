8985adf3ab6ea6524a08ad4a4111e24b2d23b0c8
enesaktay <aktayen@gmail.com>
2018-03-05 13:04:52 +0100

    add vote table to migrations

--- app/DoctrineMigrations/Version20180305114151.php
+++ app/DoctrineMigrations/Version20180305114151.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20180303020740 extends AbstractMigration
+class Version20180305114151 extends AbstractMigration
 {
     /**
      * @param Schema $schema
@@ -18,6 +18,7 @@ class Version20180303020740 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('CREATE TABLE vote (id INT AUTO_INCREMENT NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, poll CHAR(20) COLLATE utf8_bin NOT NULL COMMENT \'(DC2Type:shortid)\', answer_array_id INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE poll (id CHAR(20) COLLATE utf8_bin NOT NULL COMMENT \'(DC2Type:shortid)\', created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, question VARCHAR(100) NOT NULL, answer LONGTEXT NOT NULL COMMENT \'(DC2Type:json_array)\', visible_to_public TINYINT(1) NOT NULL, is_active TINYINT(1) NOT NULL, expiration_date DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
     }
 
@@ -29,6 +30,7 @@ class Version20180303020740 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('DROP TABLE vote');
         $this->addSql('DROP TABLE poll');
     }
 }
