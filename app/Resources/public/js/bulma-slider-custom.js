05b5952bda1349a233fb0debc39dbf69435fe20a
enesaktay <aktayen@gmail.com>
2018-03-05 13:05:41 +0100

    customized slider

--- app/Resources/public/js/bulma-slider-custom.js
+++ app/Resources/public/js/bulma-slider-custom.js
@@ -1,78 +1,85 @@
 (function () {
-'use strict';
+    'use strict';
 
 // Find output DOM associated to the DOM element passed as parameter
-function findOutputForSlider(element) {
-  var idVal = element.id;
-  var outputs = document.getElementsByTagName('output');
-  // var outputs = $('output');
-  for (var i = 0; i < outputs.length; i++) {
-    if (outputs[i].htmlFor == idVal)
-      return outputs[i];
+    function findOutputForSlider(element) {
+        var idVal = element.id;
+        var outputs = document.getElementsByTagName('output');
+        // var outputs = $('output');
+        for (var i = 0; i < outputs.length; i++) {
+            if (outputs[i].htmlFor == idVal)
+                return outputs[i];
+        }
     }
-  }
 
-function getSliderOutputPosition(slider) {
-  // Update output position
-  var newPlace,
-    minValue;
+    function getSliderOutputPosition(slider) {
+        // Update output position
+        var newPlace,
+            minValue;
 
-  var style = window.getComputedStyle(slider, null);
-  // Measure width of range input
-  sliderWidth = parseInt(style.getPropertyValue('width'), 10);
+        var style = window.getComputedStyle(slider, null);
+        // Measure width of range input
+        var sliderWidth = parseInt(style.getPropertyValue('width'), 10);
 
-  // Figure out placement percentage between left and right of input
-  if (!slider.getAttribute('min')) {
-    minValue = 0;
-  } else {
-    minValue = slider.getAttribute('min');
-  }
-  var newPoint = (slider.value - minValue) / (slider.getAttribute('max') - minValue);
+        // Figure out placement percentage between left and right of input
+        if (!slider.getAttribute('min')) {
+            minValue = 0;
+        } else {
+            minValue = slider.getAttribute('min');
+        }
+        var newPoint = (slider.value - minValue) / (slider.getAttribute('max') - minValue);
 
-  // Prevent bubble from going beyond left or right (unsupported browsers)
-  if (newPoint < 0) {
-    newPlace = 0;
-  } else if (newPoint > 1) {
-    newPlace = sliderWidth;
-  } else {
-    newPlace = sliderWidth * newPoint;
-  }
-
-  return {
-    'position': newPlace + 'px'
-  }
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  // Get all document sliders
-  var sliders = document.querySelectorAll('input[type="range"].slider');
-  [].forEach.call(sliders, function(slider) {
-    var output = findOutputForSlider(slider);
-    if (output) {
-      if (slider.classList.contains('has-output-tooltip')) {
-        // Get new output positions
-        var newPosition = getSliderOutputPosition(slider);
-
-        // Set output position
-        output.style['left'] = newPosition.position;
-      }
-
-      // Add event listener to update output when slider value change
-      slider.addEventListener('input', function(event) {
-        if (event.target.classList.contains('has-output-tooltip')) {
-          // Get new output position
-          var newPosition = getSliderOutputPosition(event.target);
-
-          // Set output position
-          output.style['left'] = newPosition.position;
+        // Prevent bubble from going beyond left or right (unsupported browsers)
+        if (newPoint < 0) {
+            newPlace = 0;
+        } else if (newPoint > 1) {
+            newPlace = sliderWidth;
+        } else {
+            newPlace = sliderWidth * newPoint;
         }
 
-        // Update output with slider choicename
-          var choicenames = jQuery.parseJSON(event.target.getAttribute('data-choicenames'));
-        output.value = choicenames[event.target.value];
-      });
+        return {
+            'position': newPlace-20 + 'px'
+        }
     }
-  });
-});
+
+    document.addEventListener('DOMContentLoaded', function () {
+        // Get all document sliders
+        var sliders = document.querySelectorAll('input[type="range"].slider');
+        [].forEach.call(sliders, function (slider) {
+            var output = findOutputForSlider(slider);
+            var choicenames = jQuery.parseJSON(slider.getAttribute('data-choicenames'));
+            if (output) {
+                if (slider.classList.contains('has-output-tooltip')) {
+                    // console.log(slider);
+                    // Get new output positions
+                    var newPosition = getSliderOutputPosition(slider);
+
+                    output.style['position'] = 'relative';
+
+                    // Set output position
+                    output.style['left'] = newPosition.position;
+
+                }
+                if (slider.classList.contains('has-output')) {
+                    output.value = choicenames[slider.value];
+                }
+
+                // Add event listener to update output when slider value change
+                slider.addEventListener('input', function (event) {
+                    if (event.target.classList.contains('has-output-tooltip')) {
+                        // Get new output position
+                        var newPosition = getSliderOutputPosition(event.target);
+
+                        // Set output position
+                        output.style['left'] = newPosition.position;
+                    }
+
+                    // Update output with slider choicename
+                    output.value = choicenames[event.target.value];
+                });
+            }
+        });
+    });
 
 }());
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,35 +2,164 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Vote;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\RangeType;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Poll;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="index")
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+
+        $expirationArray = array(
+            '0' => '2min',
+            '1' => '10min',
+            '2' => '30min',
+            '3' => '1h',
+            '4' => '2h',
+            '5' => '4h',
+            '6' => '12h',
+            '7' => '1d',
+            '8' => '2d',
+            '9' => '3d',
+            '10' => '7d',
+            '11' => '14d',
+        );
+
+
+        $poll = new Poll();
+
+        $form = $this->createFormBuilder($poll)
+            ->add("question", TextType::class)
+            ->add("visibleToPublic", CheckboxType::class)
+            ->add("isActive", CheckboxType::class)
+            ->add("expirationDate", RangeType::class, array(
+                'attr' => array(
+                    'min' => min(array_keys($expirationArray)),
+                    'max' => max(array_keys($expirationArray)),
+                    'class' => 'slider has-output',
+                    'empty_data' => '0',
+                    'value' => '0',
+                    'data-choicenames' => json_encode($expirationArray),
+                )
+            ))
+            ->add("answer", CollectionType::class, array(
+                // each entry in the array will be an "answer" field
+                'entry_type' => TextType::class,
+                'allow_add' => true,
+                // these options are passed to each "answer" type
+                'entry_options' => array(//                    'attr' => array('class' => 'email-box'),
+                ),
+            ))
+            ->add('save', SubmitType::class, array('label' => 'Create Poll'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $expirationTimesArray = array(
+                '0' => '2',
+                '1' => '10',
+                '2' => '30',
+                '3' => '60',
+                '4' => '120',
+                '5' => '240',
+                '6' => '720',
+                '7' => '1440',
+                '8' => '2880',
+                '9' => '4320',
+                '10' => '10080',
+                '11' => '20160',
+            );
+
+            $poll = $form->getData();
+
+            $nowDateTime = new \DateTime();
+            $minutesToAdd = $expirationTimesArray[$poll->getExpirationDate()];
+            $nowDateTime->modify("+{$minutesToAdd} minutes");
+
+            $poll->setExpirationDate($nowDateTime);
+
+            // ... perform some action, such as saving the task to the database
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($poll);
+            $entityManager->flush();
+            return $this->redirectToRoute('viewPoll', array(
+                "pollId" => $poll->getId()
+            ));
+        }
+
+        return $this->render('poll/index.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 
     /**
-     * @Route("/{updateId}", name="updatePoll")
+     * @Route("/poll/{pollId}", name="viewPoll")
      */
-    public function updatePollAction(Request $request, $updateId)
+    public function viewAction(Request $request, $pollId)
     {
-//        // replace this example code with whatever you need
-//        return $this->render('default/index.html.twig', [
-//            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-//        ]);
+        $poll = $this->getDoctrine()
+            ->getRepository(Poll::class)
+            ->find($pollId);
 
-        return $this->render('default/updateId.html.twig', [
-            'updateId' => $updateId
-        ]);
+//        $vote = new Vote();
+//        $vote->setPoll($poll->getId());
+//
+//        $entityManager = $this->getDoctrine()->getManager();
+//        $entityManager->persist($vote);
+//        $entityManager->flush();
+
+        $votes = $this->getDoctrine()
+            ->getRepository(Vote::class)
+            ->findBy([
+                'poll' => $poll->getId()
+            ]);
+
+        dump($votes);
+        exit;
+
+        if (!$poll) {
+            throw $this->createNotFoundException(
+                'No poll found for id ' . $pollId
+            );
+        }
+
+        $expirationDate = $poll->getExpirationDate();
+        $nowDate = new \DateTime('now');
+
+        if ($expirationDate <= $nowDate) {
+            return $this->render('poll/result.html.twig', array(
+                'poll' => $poll
+            ));
+        }
+
+        $defaultData = array('message' => 'Type your message here');
+        $form = $this->createFormBuilder($defaultData)
+            ->add('name', ChoiceType::class, array(
+                'choices' => array_flip($poll->getAnswer()),
+            ))
+            ->add('send', SubmitType::class)
+            ->getForm();
+
+        return $this->render('poll/show.html.twig', array(
+            'poll' => $poll,
+            'form' => $form->createView(),
+        ));
     }
+
 }
